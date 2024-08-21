cc1314d2490fa45184fe93be08fa11be61919050
B. Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2024-08-21 15:08:28 -0500

    Time: 6 ms (42.20%), Space: 43.5 MB (62.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> hms = new HashMap<>();
-        HashMap<Character, Integer> hmt = new HashMap<>();
-        if(s.length() != t.length())
-            return false;
-        for(int i=0;i<s.length();i++) {
-            hms.put(s.charAt(i), hms.getOrDefault(s.charAt(i),0)+1);
-            hmt.put(t.charAt(i), hmt.getOrDefault(t.charAt(i),0)+1);
+        int[] letters = new int[26];
+        for(char c: s.toCharArray())
+            letters[c-'a']++;
+        for(char c: t.toCharArray())
+            letters[c-'a']--;
+        for(int val:letters){
+            System.out.println(val);
+            if(val!=0)
+                return false;
         }
-        return hms.equals(hmt);
+        return true;
     }
 }
