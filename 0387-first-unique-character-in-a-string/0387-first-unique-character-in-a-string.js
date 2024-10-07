4eba60eb92d740ac08224fada04a7d1559da5c74
B. Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2024-10-07 08:41:01 -0500

    Time: 89 ms (65.08%), Space: 54.1 MB (43.33%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -4,19 +4,10 @@
  */
 var firstUniqChar = function(s) {
     let mp = new Map();
-    for(let ch of s) {
+    for(let ch of s) 
         mp.set(ch, (mp.get(ch) || 0)+1)
-    }
-    let unq = ''
-    for(let [k,v] of mp) {
-        if(v == 1) {
-            unq = k
-            break;
-        }
-    }
-    for(let i=0;i<s.length;i++) {
-        if(s[i] == unq)
+    for(let i=0;i<s.length;i++) 
+        if(mp.get(s[i]) == 1)
             return i;
-    }
     return -1;
 };
