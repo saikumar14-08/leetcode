d6a7f9a0b51b448f1459f4e9b167fe7be2f8e716
B. Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2024-12-16 04:36:23 -0600

    Time: 67 ms (7.97%), Space: 51.3 MB (67.05%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
@@ -0,0 +1,6 @@
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+    let newArr: number[] = [];
+    for(let i=0;i<arr.length;i++)
+        newArr.push(fn(arr[i],i));
+    return newArr;
+};
