c5265374d5a1389badc8c97e34dca90f57df7d37
B. Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2024-12-16 04:51:42 -0600

    Time: 58 ms (28.09%), Space: 49.9 MB (5.37%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const filteredArr = [];
+    for(let i=0;i<arr.length;i++) {
+        if(fn(arr[i],i) != false)
+            filteredArr.push(arr[i]);
+    }
+    return filteredArr;
+};
