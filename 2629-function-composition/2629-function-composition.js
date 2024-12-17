fc0facee563c3016e00a67ca03a04aedbd4e0de9
B. Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2024-12-17 01:02:13 -0600

    Time: 68 ms (32.78%), Space: 50.2 MB (45.58%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -4,12 +4,8 @@
  */
 var compose = function(functions) {
 
-    return function(x) {
-        let res=x;
-        for(let i=functions.length-1;i>=0;i--) {
-            res=functions[i](res); //res = 10*1
-        }
-        return res
+    return function(x) {    
+        return functions.reduceRight((x, curr) => curr(x), x)
     }
 };
 
