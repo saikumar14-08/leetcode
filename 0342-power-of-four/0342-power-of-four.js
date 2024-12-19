dd7d151376586d49820f8b0add08e98d175fe5f2
B. Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2024-12-19 04:26:09 -0600

    Time: 0 ms (100.00%), Space: 52.4 MB (74.55%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if(n === 1) return true;
+    if(n<=0 || n%4 !=0) return false;
+    
+    return isPowerOfFour(n/4)
+};
