7f75b2918e698d849117d0fce298a7726ee9d256
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-06-07 23:45:48 -0500

    Time: 5 ms (82.99%), Space: 64.6 MB (57.03%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -4,11 +4,12 @@
  */
 var isPalindrome = function(x) {
     let orig = x
-    let rev = ''
+    let rev = 0
+    if(orig < 0) return false;
     while(orig>0) {
         let rem = orig%10;
         orig = Math.floor(orig/10);
-        rev+=rem;
+        rev = (rev*10)+rem;
     }
-    return rev == x;
+    return rev === x;
 };
