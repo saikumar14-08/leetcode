dc3bf206cf91203f4f3d86d9cbf1d72de308e0a7
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-06-17 22:56:14 -0500

    Time: 0 ms (100%), Space: 57.5 MB (21.37%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,7 +3,9 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if(n==1) return true
-    if(n%2 != 0 || n<=0) return false;
-    return isPowerOfTwo(n/2)
+    while(n>=1) { 
+        if(n==1) return true;
+        return isPowerOfTwo(n/2)
+    }
+    return false;
 };
