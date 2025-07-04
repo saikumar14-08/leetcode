6b298ae1c46969ace8975464484518e9b0ca4d4a
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-07-04 16:27:35 -0500

    Time: 0 ms (100%), Space: 53.7 MB (36.32%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.js
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.js
@@ -10,21 +10,9 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
-    // let curr = head;
-    // let size = 0;
-    // while(curr.next != null) {
-    //     curr = curr.next;
-    //     size++;
-    // }
-    // let midInd = Math.ceil(size/2);
-    // let midEl = head;
-    // for(let i=0;i<midInd;i++) {
-    //     midEl = midEl.next;
-    // }
-    // return midEl;
-    let fast = head;
     let slow = head;
-    while(fast != null && fast.next != null) {
+    let fast = head;
+    while(fast && fast.next) {
         slow = slow.next;
         fast = fast.next.next;
     }
