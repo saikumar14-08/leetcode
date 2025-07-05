cb8eaf9b6ff40953eab7b5da492b9e4a96d31456
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-07-04 21:06:30 -0500

    Time: 54 ms (47.21%), Space: 59.1 MB (60.26%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,12 +11,15 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let curr = head;
-    let st = new Set();
-    while(curr) {
-        if(st.has(curr)) return true
-        st.add(curr);
-        curr = curr.next;
+    let slow = head;
+    let fast = head;
+    while(fast&&fast.next) {
+        // if(slow === fast) return true;
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow === fast) return true;
     }
     return false;
 };
+
+[-21,10,17,8,4,26,5,35,33,-7,-16,27,-12,6,29,-12,5,9,20,14,14,2,13,-24,21,23,-21,5]
