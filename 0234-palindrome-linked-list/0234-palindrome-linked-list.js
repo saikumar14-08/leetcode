bb824afb0fce1c432a817e00671bf401dff20ba8
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-07-06 11:05:39 -0500

    Time: 234 ms (5.1%), Space: 85.2 MB (17.61%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -10,26 +10,17 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    let slow = head;
-    let fast = head;
-    while(fast != null && fast.next != null) {
-        slow = slow.next;
-        fast = fast.next.next;
+    let ar = [];
+    while(head) {
+        ar.push(head.val);
+        head = head.next;
     }
-    let prev = null;
-    let curr = slow
-    while(curr) {
-        let temp = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = temp;
-    }
-    let f = head;
-    let l = prev;
-    while(l) {
-        if(f.val!=l.val) return false;
-        f = f.next;
-        l = l.next;
+    let f=0;
+    let l=ar.length-1;    
+    while(f<=l) {
+        console.log(ar[f], ar[l]);
+        if(ar[f] != ar[l]) return false;
+        f++; l--;
     }
     return true;
 };
