387d0109be99ae0fd32df1b97e5aae69fdea2c73
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-07-06 21:25:59 -0500

    Time: 0 ms (100%), Space: 57.9 MB (36.76%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -11,10 +11,8 @@
  */
 var deleteDuplicates = function(head) {
     let curr = head;
-    while(curr && curr.next) {
-        console.log(curr)
+    while(curr && curr.next) 
         if(curr.val === curr.next.val) curr.next = curr.next.next
         else curr = curr.next;
-    }
     return head;
 };
