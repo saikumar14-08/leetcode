1eee52d95563b7d45a7b0916ae7e019d18ba75bc
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-07-10 19:42:23 -0500

    Time: 0 ms (100%), Space: 54.1 MB (40.15%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -4,30 +4,36 @@
  *     this.val = (val===undefined ? 0 : val)
  *     this.next = (next===undefined ? null : next)
  * }
+
+ // Find the length of the list.
+ // Insert the sentinel node and point to head.
+ ca;cu;ate the insertion point i.e., pos-n
+ iterate till the pos like prev= prev.next and after exiting loop prev.next = prev.next.next 
+ return sentinel.next
  */
+
+ 
 /**
  * @param {ListNode} head
  * @param {number} n
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    // Create a sentinel node which points to head.
     let sentinel = new ListNode();
     sentinel.next = head;
 
-    // Calculate the length.
-    let len = 0;
     let curr = head;
+    let len = 0;
     while(curr) {
         len++;
-        curr = curr.next
+        curr =  curr.next;
     }
 
-    // Find the prev pos of the deleting element and Run the loop to find the prev.
+    let pos = len-n;
     let prev = sentinel;
-    let pos = len - n;
-    for(let i=0;i<pos;i++)
+    for(let i=0;i<pos;i++) {
         prev = prev.next;
+    }
     prev.next = prev.next.next;
-    return sentinel.next
+    return sentinel.next;
 };
