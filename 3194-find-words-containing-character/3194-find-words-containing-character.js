b777e2b811fc745fadf131bfb7cf37509b315948
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-07-15 21:31:33 -0500

    Time: 2 ms (42.11%), Space: 58.7 MB (31.27%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.js
+++ 3194-find-words-containing-character/3194-find-words-containing-character.js
@@ -5,8 +5,11 @@
  */
 var findWordsContaining = function(words, x) {
     let resAr = [];
-    for(let i=0;i<words.length;i++) {
-        if(words[i].includes(x)) resAr.push(i)
-    }
+    for(let i=0;i<words.length;i++)
+        for(let j=0;j<words[i].length;j++)
+            if(words[i][j] === x) {
+                resAr.push(i)
+                break;
+            }
     return resAr;
 };
