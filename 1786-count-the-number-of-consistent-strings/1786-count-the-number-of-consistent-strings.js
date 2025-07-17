397e2ed27281689e6f06f50856b50ba8a51b8424
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-07-16 23:55:15 -0500

    Time: 82 ms (21.82%), Space: 67.1 MB (22.2%) - LeetHub

---
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+var countConsistentStrings = function(allowed, words) {
+    const allowedSet = new Set(allowed);
+    let count = 0;
+    for (const word of words) {
+        const isValid = [...word].every(char => allowedSet.has(char));
+        if (isValid) count++;
+    }
+    return count;
+};
