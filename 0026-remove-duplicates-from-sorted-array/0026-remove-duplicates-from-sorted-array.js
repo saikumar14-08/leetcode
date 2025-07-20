a7c248237d14e496b59c1e06136bfa39e54cccee
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-07-20 11:14:16 -0500

    Time: 0 ms (100%), Space: 58.1 MB (39.44%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -1,10 +1,6 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var removeDuplicates = function(nums) {
     let pos = 0; // 0,1,
-    for(let i=0;i<nums.length;i++) {
+    for(let i=1;i<nums.length;i++) {
         if(nums[i] > nums[pos]) {
             pos=pos+1;
             nums[pos] = nums[i];   
