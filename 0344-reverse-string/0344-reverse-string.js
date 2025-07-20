fb26214e78085a4d727f99f6f178e319e4ae056b
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-07-20 11:42:02 -0500

    Time: 1 ms (54.94%), Space: 62.1 MB (16.89%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,10 +3,13 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
+    // Remember to iterate the length/2 because if you iterate throught the length then we reverse the elements twice and return the original array itself.
+    
     for(let i=0;i<s.length/2;i++) {
         let temp = s[i];
         s[i] = s[s.length-i-1];
         s[s.length-i-1] = temp;
+        
     }
-    return s
+    return s;
 };
