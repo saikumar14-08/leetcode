72134ee1602c14bf83219473803b23756b1d2c10
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-07-20 20:13:27 -0500

    Time: 4 ms (18.37%), Space: 59.9 MB (62.67%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -3,17 +3,11 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function(nums) {
-    let currCnt = 0;
-    let maxCnt = 0
-    for(let i=0;i<nums.length;i++) {
-        if(nums[i] === 0) {
-            currCnt = 0; 
-        } else if(nums[i] === 1){
-            currCnt=currCnt+1;
-            maxCnt = Math.max(currCnt,maxCnt); 
-        }
-        console.log(maxCnt);
+    let cnt = 0;
+    let max = 0;
+    for(let el of nums) {
+        el === 1 ? cnt++ : cnt = 0;
+        max = Math.max(max,cnt) // 3
     }
-    return maxCnt;
+    return max;
 };
-// [1,1,1,0,1]
