9d52612398231bd984fcee680fa68466dc1c3879
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2025-07-20 19:53:34 -0500

    Time: 2 ms (54.54%), Space: 58.8 MB (90.68%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,21 +2,18 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-    let f = 0;
-    let s = 0;
-    if(nums.length == 1 && nums[0] != 0) 
-        return nums;
-    while(s<nums.length) { // 0, 1, 
-        if(nums[s] === 0) { // 
-            s++;
-        } else {
-            let temp = nums[s];
-            nums[s] = nums[f];
-            nums[f] = temp;
-            f++;
-            s++
-        }
+ /**
+ If your ith el is 0 then find the next non zero element an du
+  */
+var moveZeroes = function(n) {
+    let pos = 0;
+    for(let i=0;i<n.length;i++) {
+        if(n[i] != 0) {
+            n[pos] = n[i];
+            pos++;
+        } 
     }
-    return nums
+    for(let i=pos;i<n.length;i++) {
+        n[i] = 0;
+    }
 };
