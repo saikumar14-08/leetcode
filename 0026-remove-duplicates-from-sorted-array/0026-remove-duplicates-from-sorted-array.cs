07f255381793a34c608486901bf657fee233e0e6
Sai Kumar Reddy <52173530+saikumar14-08@users.noreply.github.com>
2026-08-16 23:01:03 -0500

    Time: 0 ms (100%), Space: 51 MB (82.14%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -1,9 +1,11 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
         int pos=0;
-        for(int i=0;i<nums.Length;i++) {
-            if(nums[i] >nums[pos]) 
-                nums[++pos] = nums[i];
+        for(var i=0;i<nums.Length;i++) {
+            if(nums[i] > nums[pos]) {
+                pos+=1;
+                nums[pos] = nums[i];
+            }
         }
         return pos+1;
     }
